f8ba70b4f8e3ed3cd3308e2c24a041f7598991d3
Franklin Hu <franklin.who@gmail.com>
2011-10-18 08:55:39 -0700

    Add gitignore, subversion to setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 # Create symbolic links
 ln -s .bash_aliases ~/.bash_aliases
 ln -s .gitconfig ~/.gitconfig
+ln -s .gitignore ~/.gitignore
 ln -s .inputrc ~/.inputrc
 ln -s .vimrc ~/.vimrc
 
-sudo apt-get install git gnome-do
+sudo apt-get install git gnome-do subversion
